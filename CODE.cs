8b58bb8f39c154b426acb61df44568086c08cd92
Boitumelo-boop <boitumelotautumi@gmail.com>
2024-06-21 09:50:06 +0200

    Add files via upload

---
+++ CODE.cs
@@ -0,0 +1,21 @@
+﻿public class Converter
+{
+    public double ConvertKilometersToMiles(double kilometers)
+    {
+        double conversionFactor = 0.621371;
+        double miles = kilometers * conversionFactor;
+        return miles;
+    }
+}
+public class VATCalculator
+{
+    public double CalculateTotalAmountWithVAT(double amount)
+    {
+        double vatRate = 0.15;
+        double vatAmount = amount * vatRate;
+        double totalAmountWithVAT = amount + vatAmount;
+        return totalAmountWithVAT;
+    }
+}
+
+
